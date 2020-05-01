1baf986fc0415554af94ae92f4dbbf3c43e01a70
SGCMarkus <markusornik@gmail.com>
2020-05-01 13:02:58 +0200

    RMX1921: downrev audio to 4.0
    * fixes some crashes of the audio service related to setMicrophoneFieldDimension which is an extension in 5.0

--- device.mk
+++ device.mk
@@ -28,8 +28,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.soundtrigger@2.2-service \
