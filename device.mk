f3f061ace137cedd7fce8d2fcef9314938f94777
ThatisJigen <gianfranco.liguori.96@gmail.com>
2020-05-19 15:54:40 +0200

    RMX1921: move touch and fod related hals to vendor also build enforcing as policies are fixed

--- device.mk
+++ device.mk
@@ -181,7 +181,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xt \
-    lineage.biometrics.fingerprint.inscreen@1.1-service.xt \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.1-service.xt \
     vendor.oppo.hardware.biometrics.fingerprint@2.1
 
 PRODUCT_COPY_FILES += \
@@ -419,7 +419,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    lineage.touch@1.0-service.xt
+    vendor.lineage.touch@1.0-service.xt
 
 # Trust HAL
 PRODUCT_PACKAGES += \
