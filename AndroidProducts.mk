3357a02534e23f4034001058f422963c42b60765
Vishnu U <vishnuedavarath@gmail.com>
2020-06-03 18:33:32 +0530

    Add missing operator

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/colt_RMX1921.mk
-    
-COMMON_LUNCH_CHOICES :=
+
+COMMON_LUNCH_CHOICES := \
     colt_RMX1921-eng\
     colt_RMX1921-user\
     colt_RMX1921-userdebug
