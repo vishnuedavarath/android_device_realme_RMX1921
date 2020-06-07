af15a86672fd25dfbddb30cf2d9fed718253bc7b
Vishnu U <vishnuedavarath@gmail.com>
2020-06-07 15:47:19 +0530

    carbonify

--- carbon_RMX1921.mk
+++ carbon_RMX1921.mk
@@ -4,11 +4,19 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common AEX stuff.
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common CARBON stuff.
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# WITH_GAPPS := true
+# TARGET_GAPPS_ARCH := arm64
+# IS_PHONE := true
+
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 2280
